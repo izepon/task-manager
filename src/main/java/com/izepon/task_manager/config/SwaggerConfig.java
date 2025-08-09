8a572a65e376bff651b3fa2ba383d2cb4bdfba22
Jean Izepon <izeponjc@gmail.com>
2025-08-08 21:51:30 -0300

    ajustes gerais no funcionamento

--- src/main/java/com/izepon/task_manager/config/SwaggerConfig.java
+++ src/main/java/com/izepon/task_manager/config/SwaggerConfig.java
@@ -1,22 +1,13 @@
 package com.izepon.task_manager.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
-    private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                .bearerFormat("JWT")
-                .scheme("bearer");
-    }
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -25,8 +16,6 @@ public class SwaggerConfig {
                         .title("Task Manager API")
                         .version("1.0")
                         .description("API for internal task management.")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
